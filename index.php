1fd025e6db211d59d7671a3f75cfdd6b57111ac9
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-12-13 13:24:37 +0100

    Aggiunge stampa paragrafo e la sua lunghezza Paragrafo: $text

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     $title = "PHP Badwords";
+    $text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur iusto ipsa excepturi perspiciatis quam corrupti rem eaque atque! Deleniti amet ut ipsum quidem provident ullam nulla fugiat molestias deserunt possimus!";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,5 +12,7 @@
 </head>
 <body>
     <h1><?php echo $title; ?></h1>
+    <p><?php echo $text; ?></p>
+    <h2>Lunghezza paragrafo con badwords: <?php echo strlen($text); ?></h2>
 </body>
 </html>
