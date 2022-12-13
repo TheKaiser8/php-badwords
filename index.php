b48c4695d8c694683f174fc0e6483f3e001eaf92
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-12-13 13:17:05 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $title = "PHP Badwords";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+</body>
+</html>
